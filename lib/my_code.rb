e429cafb8959c8cbbdc1f1daca1bfe9278a50512
bryan lesht <bryan.lesht@colorado.edu>
2020-02-19 03:30:22 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
-# Your Code Here
+def map(array)
+  new_array = []
+  index = 0 
+  while index < array.length do 
+    
+    new_array << yield(array[index])
+    index += 1
+    
+  end 
+  new_array
+end
+
+
+def reduce(array,start=nil)
+  if start
+    total = start 
+    i = 0 
+  else
+    total = array[0]
+    i = 1 
+  end 
+  while i < array.length do 
+    total = yield(total, array[i])
+    i += 1 
+  end 
+    total 
+    
+    
+end 

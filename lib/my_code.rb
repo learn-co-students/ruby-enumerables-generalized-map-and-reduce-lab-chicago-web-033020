9e79e34b4d8fd993220dc19c104a9e4566e17efd
Shau Yin Luk <ivan.luk@icloud.com>
2020-02-27 16:45:42 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
 # Your Code Here
+def map (source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push(yield(source_array[i])) 
+    i += 1
+  end
+  return new
+end
+
+def reduce(source_array, starting_point=nil)
+  i = 0
+  num = 0
+  if starting_point 
+    num = starting_point
+  else
+    num = source_array[0]
+    i = 1
+  end
+  while i < source_array.size do
+    num =  yield(num, source_array[i])
+    i += 1
+  end
+  num
+end

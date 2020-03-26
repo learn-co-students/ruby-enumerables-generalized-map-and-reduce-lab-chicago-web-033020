d4cef71087361b4df9fbe70dc01f093ee53ad0e8
Julian Antonio Smith <juliansmith94@hotmail.com>
2020-03-26 22:40:47 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
-# Your Code Here
+def map(array)
+    #the method "map" returns a new array of manipulated elements
+    new = []
+    i = 0
+    while i < array.length 
+        new.push(yield(array[i]))
+        i += 1
+    end
+    new
+end
+
+def reduce(array, starting_point = nil)
+  #the method "reduce" returns an array of condensed elements into one value
+  if starting_point
+    sum = starting_point
+    i = 0 
+  else
+    sum = array[0]
+    i = 1 
+  end
+  
+  while i < array.length
+  sum = yield(sum, array[i])
+  i += 1
+  end
+  sum
+
+end
